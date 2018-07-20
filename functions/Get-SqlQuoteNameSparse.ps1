0cf1c307ca79fe167caed56d522e556be38a8ff6
Peter Vandivier <petervandivier@gmail.com>
2018-07-20 08:17:27 +0100

    do not require quote for hash character "#"

--- functions/Get-SqlQuoteNameSparse.ps1
+++ functions/Get-SqlQuoteNameSparse.ps1
@@ -1,7 +1,7 @@
 function Get-SqlQuoteNameSparse {
 <#
     .SYNOPSIS
-        Approximate match for ssms highlight engine. Quotes reserved & non-reserved keywords. 
+        Approximate match for ssms highlight engine. Quotes reserved & non-reserved keywords.
         Also quotes any name that matches quotename requirements.
         Approximately: any text containing a non alphanumeric character or beginning with a number UNLESS already quoted.
 
@@ -9,27 +9,27 @@ function Get-SqlQuoteNameSparse {
         https://docs.microsoft.com/en-us/sql/relational-databases/databases/database-identifiers
 
     .EXAMPLE
-        @("Column1","1Column","_1Column","Foo Bar","Transaction","Test"," Blah","[ Bork") ` 
+        @("Column1","1Column","_1Column","Foo Bar","Transaction","Test"," Blah","[ Bork") `
         | % {Get-SqlQuoteNameSparse $_}
  #>
     [cmdletbinding()]Param(
         [parameter(
-            Position=0, 
-            Mandatory=$true, 
+            Position=0,
+            Mandatory=$true,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
             [Alias('word')]
             [AllowEmptyString()]
             [string]$text
     )
-    
+
     if(-not [string]::IsNullOrEmpty($text)){
         $colorWords = (Get-SqlKeyWords).word;
         $needsQuote = $false
 
-        if($colorWords -Contains $text){$needsQuote=$true} 
-        if($text -match "[^a-zA-Z0-9_]"){$needsQuote=$true} 
-        if($text[0] -match "[0-9]"){$needsQuote=$true} 
+        if($colorWords -Contains $text){$needsQuote=$true}
+        if($text -match "[^a-zA-Z0-9#_]"){$needsQuote=$true}
+        if($text[0] -match "[0-9]"){$needsQuote=$true}
 
         if($text.StartsWith("[") -and $text.EndsWith("]")){$needsQuote=$false}
 
@@ -39,6 +39,6 @@ function Get-SqlQuoteNameSparse {
     }
 
     [pscustomobject]@{
-        Text = $text 
+        Text = $text
     }
 }
